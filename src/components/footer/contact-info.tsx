f4b26cab12222b353983c41e42566eb89c012c74
tigeryash <yashwanth.venkatesan@gmail.com>
2024-05-05 05:33:08 -0400

    used next Links for footer socials

--- src/components/footer/contact-info.tsx
+++ src/components/footer/contact-info.tsx
@@ -3,6 +3,7 @@ import H3 from "../H3";
 import { HiLocationMarker } from "react-icons/hi";
 import { MdMarkEmailUnread } from "react-icons/md";
 import { FaPhoneVolume } from "react-icons/fa6";
+import Link from "next/link";
 
 const ContactInfo = () => {
   return (
@@ -13,17 +14,27 @@ const ContactInfo = () => {
         <div className="flex items-center justify-center md:justify-normal">
           <FaPhoneVolume className="w-4 h-4 md:w-5 md:h-5" />
 
-          <span className="ml-2">+1 123 456 7890</span>
+          <Link href="tel:+11234567890" className="ml-2">
+            +1 123 456 7890
+          </Link>
         </div>
 
         <div className="flex items-center justify-center md:justify-normal">
           <MdMarkEmailUnread className="w-4 h-4 md:w-5 md:h-5" />
-          <span className="ml-2 underline">Example@email.com </span>
+          <Link href="mailto:example@email.com" className="ml-2 underline">
+            Example@email.com{" "}
+          </Link>
         </div>
 
         <div className="flex items-center justify-center md:justify-normal">
           <HiLocationMarker className="w-4 h-4 md:w-5 md:h-5" />
-          <span className="ml-2">1234 Street Name, City Name, State</span>
+          <Link
+            href="https://www.google.com/maps/search/?api=1&query=1234+Street+Name,+City+Name,+State"
+            target="_blank"
+            className="ml-2"
+          >
+            1234 Street Name, City Name, State
+          </Link>
         </div>
       </div>
     </div>
--- src/components/footer/socials.tsx
+++ src/components/footer/socials.tsx
@@ -1,37 +1,38 @@
 import { InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 import { BsTwitterX } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa6";
 
 const Socials = () => {
   return (
     <div className="flex justify-center space-x-1 md:space-x-3 lg:space-x-5 ">
-      <a
+      <Link
         href="#"
         className="group p-1 md:p-2 lg:p-4 rounded-full border hover:scale-[1.3] flex items-center justify-center hover:bg-white hover:border-black transition-all duration-300"
       >
         <FaFacebookF className="group-hover:text-black lg:w-7 lg:h-7 text-white" />
-      </a>
+      </Link>
 
-      <a
+      <Link
         href="#"
         className="group p-1 md:p-2 lg:p-4 rounded-full border hover:scale-[1.3] flex items-center justify-center hover:bg-white hover:border-black transition-all duration-300"
       >
         <InstagramLogoIcon className="group-hover:text-black lg:w-7 lg:h-7 text-white" />
-      </a>
+      </Link>
 
-      <a
+      <Link
         href="#"
         className="group p-1 md:p-2 lg:p-4 rounded-full border hover:scale-[1.3] flex items-center justify-center hover:bg-white hover:border-black transition-all duration-300"
       >
         <LinkedInLogoIcon className="group-hover:text-black lg:w-7 lg:h-7 text-white" />
-      </a>
+      </Link>
 
-      <a
+      <Link
         href="#"
         className="group p-1 md:p-2 lg:p-4 rounded-full border hover:scale-[1.3] flex items-center justify-center hover:bg-white hover:border-black transition-all duration-300"
       >
         <BsTwitterX className="group-hover:text-black lg:w-7 lg:h-7 text-white" />
-      </a>
+      </Link>
     </div>
   );
 };
